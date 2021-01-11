ace12c5cd3e146489e21e6591bc1c889184e2856
ericav99 <ericav99@email.arizona.edu>
2021-01-11 13:46:18 -0800

    first

---
+++ testProject_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/reu-cat/testProject_ws/src
+# Build directory: /home/reu-cat/testProject_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
