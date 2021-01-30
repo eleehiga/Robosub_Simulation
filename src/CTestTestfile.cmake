67eaa09023a50e9693e6ad20dfcab957954bc611
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-01-30 13:50:14 -0800

    compiled cmakelists.txt

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nick*/ProofOfConcept/src
+# Build directory: /home/nick*/ProofOfConcept/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
