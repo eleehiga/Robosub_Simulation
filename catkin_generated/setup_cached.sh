c1a213efaaf98cf0a7984065edc42df367b3f337
Jason Lunder <jlunder@zagmail.gonzaga.edu>
2021-03-27 12:32:14 -0700

    more files

---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PATH='/opt/ros/noetic/bin:/usr/bin:/bin:/home/toast/anaconda3/bin:/home/toast/anaconda3/condabin:/home/toast/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/toast/bin'
