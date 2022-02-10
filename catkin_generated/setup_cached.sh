3049f4e6ef72e54e82a7a9e31bfcfa953f52f15f
eleehiga <eleehiga808@gmail.com>
2022-02-10 17:01:46 +0000

    Added launch files, model, and plugins for the depth camera

--- catkin_generated/setup_cached.sh
+++ catkin_generated/setup_cached.sh
@@ -10,5 +10,5 @@
 export CMAKE_PREFIX_PATH="/Robosub_Simulation/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/Robosub_Simulation/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/Robosub_Simulation/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
-export ROSLISP_PACKAGE_DIRECTORIES='/Robosub_Simulation/devel/share/common-lisp'
+export ROSLISP_PACKAGE_DIRECTORIES="/Robosub_Simulation/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/toast/Documents/RoboSub/Machine_Learning_2020-2021/ProofOfConcept:/home/toast/Documents/RoboSub/ProofOfConcept:/Robosub_Simulation:$ROS_PACKAGE_PATH"
